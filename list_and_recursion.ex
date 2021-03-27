90dda34d3180eaefed49572a20bd70c8823b0966
Lucas Firmo <lucasmfir@gmail.com>
2021-03-27 00:07:52 -0300

    list exercices

--- list_and_recursion.ex
+++ list_and_recursion.ex
@@ -5,8 +5,31 @@
 # ​ 	​iex>​ MyList.mapsum [1, 2, 3], &(&1 * &1)
 # ​ 	14
 
+# 2
+# Write a max(list) that returns the element with the maximum value in the list.
+# (This is slightly trickier than it sounds.)
+
+# 3
+# An Elixir single-quoted string is actually a list of individual character codes.
+# Write a caesar(list, n) function that adds n to each list element, wrapping if the addition results in a character greater than z.
+
+# ​ 	​iex>​ MyList.caesar(​'ryvkve'​, 13)
+# ​ 	?????? :)
+
 defmodule MyList do
   def mapsum([], _func), do: 0
 
   def mapsum([head | tail], func), do: func.(head) + mapsum(tail, func)
+
+  def max_val([x]), do: x
+
+  def max_val([head | tail]), do: Kernel.max(head, max_val(tail))
+
+  def caesar([], _n), do: []
+
+  def caesar([head | tail], n) when head + n <= ?z do
+    [head + n | caesar(tail, n)]
+  end
+
+  def caesar([head | tail], n), do: [head + n - 26 | caesar(tail, n)]
 end
